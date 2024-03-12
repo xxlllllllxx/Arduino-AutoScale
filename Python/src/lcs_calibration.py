ca3b36285e55f113cb17ee4ae724af44b1cb92d1
Lewismasallo <lewismasallo@gmail.com>
2024-03-13 07:04:04 +0800

    public

--- Python/src/lcs_calibration.py
+++ Python/src/lcs_calibration.py
@@ -102,7 +102,3 @@ class UI:
 # ADD weight
 # 1kg value -> v1 = value (with 1kg)
 # 2kg value -> v2 = value (with 2kg)
-# calibration -> linear = (v2 == v1*2)  ----> end
-# calibration -> with_eq =  (v2 == v1 + newval)
-# 3kg value -> v3 = value (with 3kg)
-# calibration -> with_eq = (v1 = val) (v2 == v1 + val/2) (v3 == v2 + val/3)
